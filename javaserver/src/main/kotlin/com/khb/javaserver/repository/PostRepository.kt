337234acc1788243347dd9e3431ff8276d323d3c
김호빈 <hobeen@192.168.nate.com>
2025-01-16 21:37:47 +0900

    javaserver update

--- javaserver/src/main/kotlin/com/khb/javaserver/repository/PostRepository.kt
+++ javaserver/src/main/kotlin/com/khb/javaserver/repository/PostRepository.kt
@@ -3,5 +3,4 @@ package com.khb.javaserver.repository
 import com.khb.javaserver.entity.Post
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface PostRepository: JpaRepository<Post, Long> {
-}
+interface PostRepository: JpaRepository<Post, Long>
