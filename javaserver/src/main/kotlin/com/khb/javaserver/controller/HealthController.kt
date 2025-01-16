3cdf0af0038c2b1d8ea93969e905d2215fdb4088
김호빈 <hobeen@192.168.nate.com>
2025-01-16 22:14:43 +0900

    add: java server healthController

---
+++ javaserver/src/main/kotlin/com/khb/javaserver/controller/HealthController.kt
@@ -0,0 +1,29 @@
+package com.khb.javaserver.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping
+class HealthController {
+
+    @GetMapping("/")
+    fun healthCheck(): String {
+        return "OK"
+    }
+
+    @GetMapping("/info")
+    fun info(): Map<String, String> {
+        // 환경 변수에서 Pod 이름 가져오기
+        val podName = System.getenv("HOSTNAME") ?: "unknown"
+
+        // Node 이름 가져오기
+        val nodeName = System.getenv("NODE_NAME") ?: "unknown"
+
+        return mapOf(
+            "pod" to podName,
+            "node" to nodeName
+        )
+    }
+}
