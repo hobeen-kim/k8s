41f38ff8df7b3f236af84eb28ada60f652ad2751
hobeen-kim <sksjsksh32@gmail.com>
2025-01-19 15:22:06 +0900

    consumer gpt update

--- k8s-java-consumer/src/main/kotlin/com/khb/k8sjavaconsumer/service/gpt/ChatGpt.kt
+++ k8s-java-consumer/src/main/kotlin/com/khb/k8sjavaconsumer/service/gpt/ChatGpt.kt
@@ -1,5 +1,8 @@
 package com.khb.k8sjavaconsumer.service.gpt
 
+import org.springframework.stereotype.Component
+
+@Component
 class ChatGpt: GptService {
     override fun summarizeArticle(text: String): String {
         return "This is a summarized article"
