e7b2536a1f66eca8a327fd9c40d12bd358d9b49f
hobeen-kim <sksjsksh32@naver.com>
2025-01-19 16:53:22 +0900

    kafka dev 용 토픽 추가

--- kafka/install-kafka.sh
+++ kafka/install-kafka.sh
@@ -20,6 +20,10 @@ echo "inter.broker.listener.name=LOCAL" >> ~/kafka/kafka_2.13-3.9.0/config/serve
 
 # 토픽 생성
 ~/kafka/kafka_2.13-3.9.0/bin/kafka-topics.sh --create --topic raw-article --bootstrap-server localhost:9092 --replication-factor 1 --partitions 3
+~/kafka/kafka_2.13-3.9.0/bin/kafka-topics.sh --create --topic refined-article --bootstrap-server localhost:9092 --replication-factor 1 --partitions 3
+## dev 용 토픽 생성
+~/kafka/kafka_2.13-3.9.0/bin/kafka-topics.sh --create --topic raw-article-dev --bootstrap-server localhost:9092 --replication-factor 1 --partitions 3
+~/kafka/kafka_2.13-3.9.0/bin/kafka-topics.sh --create --topic refined-article-dev --bootstrap-server localhost:9092 --replication-factor 1 --partitions 3
 
 # 토픽 확인
 ~/kafka/kafka_2.13-3.9.0/bin/kafka-topics.sh --list --bootstrap-server localhost:9092
